57ce9fe1ae9c1024b878a4cc7d05b5bad1a9acd4
SanderRasmus <sanderrasmussen@outlook.com>
2023-08-24 13:25:05 +0200

    Betingelser og Valg

--- Intro.py
+++ Intro.py
@@ -16,4 +16,44 @@ def kuprat():
     print('             ||----W |')
     print('             ||     ||')
 
-kuprat()
+
+
+### Oppgave 1 - Betingelser og Valg
+
+def størsttall():
+    a = int(input("Skriv inn Tall nummer 1! "))
+    størst = a
+
+    b = int(input("Skriv inn Tall nummer 2! "))
+    
+    if b > størst:
+        størst = b
+
+    c = int(input("Skriv inn Tall nummer 3! "))
+
+    if c > størst:
+        størst = c
+
+    d = int(input("Skriv inn Tall nummer 4! "))
+
+    if d > størst:
+        størst = d
+
+    e = int(input("Skriv inn Tall nummer 5! "))
+
+    if e > størst:
+        størst = e
+
+    print("Det største tallet du oppga var: " + str(størst))
+
+
+def partall():
+    tall = int(input("Skriv inn et tall: "))
+    tall = (tall % 2)
+
+    if tall == 1:
+        print("Dette tallet er et oddetall")
+    else:
+        print("Dette tallet er et partall")
+
+partall()
